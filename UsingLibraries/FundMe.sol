7852fcf77550d35bd350976d290add81914ef43a
FuyuFacu <fuyuestudios@gmail.com>
2025-02-04 10:44:57 -0300

    Create FundMe.sol

---
+++ UsingLibraries/FundMe.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimunUsd = 5e18;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimunUsd, "didn't send enough ETH");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }
+
+    
+
+}
